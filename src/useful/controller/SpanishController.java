7ecf98584920c0cea1c2ef43583fe7c64609d989
greenRose2018 <brifun13@gmail.com>
2018-02-07 08:47:43 -0700

    moved code to ArrayJava project

--- src/useful/controller/SpanishController.java
+++ src/useful/controller/SpanishController.java
@@ -2,39 +2,14 @@ package useful.controller;
 
 public class SpanishController
 {
-	private String[] myWords = {"Ryan", "mason", "Sheradon", "Preston"};
-	private int[] myNumbers;
+	
 	public SpanishController()
 	{
-		myWords = new String[6];
-		myNumbers = new int[8];
+		
 	}
 	
 	public void start()
 	{
-		System.out.println("My name is Brittney, and I am a student");
-		System.out.println(myWords);
-		System.out.println(myNumbers);
-		myWords = new String [] { "Brandon", "Zane", "Amber", "Brittney", "Jeran",  "Dylan"};
-		for (int index = 0; index < myNumbers.length; index += 1)
-		{
-			myNumbers[index] = index + 1;
-		}
 		
-		showLoopWithArray();
 	}
-	
-	private void showLoopWithArray()
-	{
-		for (String current : myWords)
-		{
-			System.out.println(current);
-		}
-		
-		for (int index = 0; index < myNumbers.length; index += 1)
-		{
-			System.out.println(myNumbers[index]);
-		}
-	}
-
 }

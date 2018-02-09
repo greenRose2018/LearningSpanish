1b76db2eba9719a8c6d75f1dea16bf95d3f4c4a6
greenRose2018 <brifun13@gmail.com>
2018-02-09 09:19:03 -0700

    set basic for the controller to work

--- src/useful/controller/SpanishController.java
+++ src/useful/controller/SpanishController.java
@@ -1,15 +1,23 @@
 package useful.controller;
 
+import vocab.view.SpanishFrame;
+import vocab.model.*;
+
 public class SpanishController
 {
+	private SpanishFrame mainFrame;
 	
 	public SpanishController()
 	{
-		
+		mainFrame = new SpanishFrame(this);
 	}
 	
 	public void start()
 	{
 		
+	}
+	public SpanishFrame getSpanishFrame()
+	{
+		return mainFrame;
 	}
 }

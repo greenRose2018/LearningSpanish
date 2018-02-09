cf40f4ae37637b089a53c446d05705694eca2a6a
greenRose2018 <brifun13@gmail.com>
2018-02-09 09:20:06 -0700

    setup basic for Frame,  getter and setters to work

--- src/vocab/view/SpanishFrame.java
+++ src/vocab/view/SpanishFrame.java
@@ -5,5 +5,35 @@ import javax.swing.JFrame;
 
 public class SpanishFrame extends JFrame
 {
+	private SpanishController appController;
+	private SpanishPanel mainMenuPanel;
+	private SpanishPanel translationPanel;
+	private SpanishPanel verbsPanel;
+	private SpanishPanel foodPanel;
+	
+	
+	public SpanishFrame(SpanishController appController)
+	{
+		super();
+		this.setAppController(appController);
+		mainMenuPanel = new SpanishPanel(appController);
+		
+		
+	}
+	private void setupFrame()
+	{
+		//this.setContentPane(mainMenuPanel);
+		
+		
+	}
+	
+	public SpanishController getAppController()
+	{
+		return appController;
+	}
+	public void setAppController(SpanishController appController)
+	{
+		this.appController = appController;
+	}
 
 }

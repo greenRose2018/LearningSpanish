c1458be350d10013f657e6cb1be62fceedeb0bc7
greenRose2018 <brifun13@gmail.com>
2018-02-12 21:36:58 -0700

    added panels like cardlayout

--- src/vocab/view/SpanishPanel.java
+++ src/vocab/view/SpanishPanel.java
@@ -15,31 +15,60 @@ public class SpanishPanel extends JPanel
 	
 	private JPanel foodPanel;
 	private JPanel verbsPanel;
+	private JPanel mainPanel;
 
 	public SpanishPanel(SpanishController appController)
 	{
 		super();
+		this.appController = appController;
 		baseLayout = new SpringLayout();
+		mainPanel = new JPanel();
 		
 		foodBtn = new JButton("Foods");
+		
+		foodBtn.setBounds(226, 100, 67, 25);
+		
 		verbsBtn = new JButton("Verbs");
+		
+		verbsBtn.setBounds(63, 100, 65, 25);
 		
 		foodPanel = new JPanel();
 		verbsPanel = new JPanel();
-
+		
+		setupPanel();
 	}
 	private void setupPanel()
 	{
 		this.setLayout(baseLayout);
-		this.add(foodBtn);
-		this.add(verbsBtn);
-		this.add(foodPanel);
-		this.add(verbsPanel);
 		
+		//mainPanel
+		this.add(mainPanel);
+		mainPanel.setLayout(null);
+		mainPanel.add(verbsBtn);
+		mainPanel.add(foodBtn);
+		
+		//FoodPanel
+		this.add(foodPanel);
+		foodPanel.setLayout(null);
+		
+		//VerbsPanel 
+		this.add(verbsPanel);
+		verbsPanel.setLayout(null);
 	}
 	private void setupLayout()
 	{
-		
+		baseLayout.putConstraint(SpringLayout.NORTH, mainPanel, 0, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, mainPanel, 0, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, mainPanel, 0, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.EAST, mainPanel, 0, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, foodBtn, 101, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.EAST, foodBtn, -314, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, verbsBtn, 0, SpringLayout.NORTH, foodBtn);
+		baseLayout.putConstraint(SpringLayout.WEST, verbsBtn, 33, SpringLayout.EAST, foodBtn);
+		baseLayout.putConstraint(SpringLayout.NORTH, foodPanel, 34, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, foodPanel, 40, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, verbsPanel, 10, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, verbsPanel, 10, SpringLayout.WEST, this);
 	}
 	private void setupListeners()
 	{

2ece11c522ea41a1db19f29f31358d1acd56c14e
greenRose2018 <brifun13@gmail.com>
2018-05-28 18:39:57 -0600

    created a class inside computerscience package
    called big data

---
+++ src/vocab/model/tech/computerscience/BigData.java
@@ -0,0 +1,40 @@
+package vocab.model.tech.computerscience;
+
+import vocab.model.TechnicalWords;
+import vocab.model.interfaces.*;
+
+public class BigData extends TechnicalWords implements Technical
+{
+//	public BigData(String englishWord, String spanishWord, String typeOfTech) 
+//	{
+//		super(englishWord, spanishWord, typeOfTech);
+//	}
+//	public BigData(String spanishWord, String typeOfTech) 
+//	{
+//		super("Big Data", spanishWord, typeOfTech);
+//	}
+//	public BigData(String typeOfTech) 
+//	{
+//		super("Big Data", spanishWord, typeOfTech);
+//	}
+	public BigData() 
+	{
+		super("Big Data", "Grandes Datos", "Computer Science");
+	}
+
+	public String englishWord() 
+	{
+		return "Big Data";
+	}
+
+	public String spanishWord() 
+	{
+		return "Grandes Datos";
+	}
+
+	public String categoryTech() 
+	{
+		return "Computer Science";
+	}
+
+}
